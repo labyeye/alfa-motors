81ca7f3ceab0f75be398389c43f3011fb69246be
Labh Bothra <labhbothra1380@gmail.com>
2025-11-07 16:59:04 +0530

    Done with Many Things

--- alfa-dashboard/frontend/src/pages/LoginPage.js
+++ alfa-dashboard/frontend/src/pages/LoginPage.js
@@ -1,16 +1,16 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import AuthContext from '../context/AuthContext';
-import AuthForm from '../components/AuthForm';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
 
 const LoginPage = () => {
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  console.log('User in LoginPage:', user); // Add this for debugging
+  console.log("User in LoginPage:", user); // Add this for debugging
 
   if (user) {
-    navigate(user.role === 'admin' ? '/admin' : '/staff', { replace: true });
+    navigate(user.role === "admin" ? "/admin" : "/staff", { replace: true });
     return null;
   }
 
